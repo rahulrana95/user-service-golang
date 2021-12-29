05e7bd039eb6b629a6043ba8bc911daca669e7a6
Rahul Rana <rahulrana@uber.com>
2021-12-29 22:29:00 +0530

    First

---
+++ cmd/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	log.Println("Starting the grpc server at Port: 5000")
+	log.Println("Starting the http server at Port: 5050")
+
+	ss, err := net.Listen("tcp", "localhost:5000")
+
+	if err != nil {
+		log.Fatalf("Failed to start server.")
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "Welcome to user service. Thank you")
+	})
+
+	s := &http.Server{
+		Addr:           ":5050",
+		Handler:        nil,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
+
+	ser := grpc.NewServer()
+
+	if err := ser.Serve(ss); err != nil {
+		log.Fatalf("Failed to serve", err)
+	}
+
+}
